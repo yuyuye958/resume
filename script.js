fb0d32813bf3024f958ab82e6cbb903221964850
Allen <yuyuye958@qq.com>
2018-09-26 19:20:51 +0800

    update

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-// loading
-setTimeout(function () {
-    welcome.classList.remove('active')
-}, 500)
-
 // 导航栏滚动变化
+// 添加 offset 类
+let specialTags = document.querySelectorAll('[data-x]')
+for (let i = 0; i < specialTags.length; i++) {
+    specialTags[i].classList.add('offset')
+}
+
+setTimeout(function () {
+    findClosest()
+}, 100)
+
 window.onscroll = function () {
     if (window.scrollY > 0) {
         topNavBar.classList.add('sticky')
     } else {
         topNavBar.classList.remove('sticky')
     }
+    findClosest()
 }
+
+function findClosest() {
+    let specialTags = document.querySelectorAll('[data-x]')
+    let minIndex = 0
+    for (let i = 1; i < specialTags.length; i++) {
+        if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
+            minIndex = i
+        }
+    }
+    // minIndex 就是距离窗口顶部最近的元素
+    specialTags[minIndex].classList.remove('offset')
+    let id = specialTags[minIndex].id
+    let a = document.querySelector('a[href="#' + id + '"]')
+    let li = a.parentNode
+    let brothersAndMe = li.parentNode.children
+    for (let i = 0; i < brothersAndMe.length; i++) {
+        brothersAndMe[i].classList.remove('highlight')
+    }
+    li.classList.add('highlight')
+}
+
+
 
 // 导航栏二级菜单
 let liTags = document.querySelectorAll('.topNavBar nav > ul > li')
@@ -25,6 +53,12 @@ for (let i = 0; i < liTags.length; i++) {
 
 // 滚动到指定元素
 let aTags = document.querySelectorAll('.topNavBar nav > ul > li > a')
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
 for (let i = 0; i < aTags.length; i++) {
     aTags[i].onclick = function (x) {
         x.preventDefault()
@@ -32,7 +66,21 @@ for (let i = 0; i < aTags.length; i++) {
         let href = a.getAttribute('href')
         let element = document.querySelector(href)
         let top = element.offsetTop
-        window.scrollTo(0, top - 70)
+
+        let currentTop = window.scrollY
+        let targetTop = top - 75
+        let s = targetTop - currentTop // 路程
+        var coords = { y: currentTop }; // 起始位置
+        var t = Math.abs((s / 100) * 300) // 时间
+        if (t > 500) { t = 500 }
+        var tween = new TWEEN.Tween(coords) // 起始位置
+            .to({ y: targetTop }, t) // 结束位置 和 时间
+            .easing(TWEEN.Easing.Cubic.InOut) // 缓动类型
+            .onUpdate(function () {
+                // coords.y 已经变了
+                window.scrollTo(0, coords.y) // 如何更新界面
+            })
+            .start(); // 开始缓动
     }
 }
 
